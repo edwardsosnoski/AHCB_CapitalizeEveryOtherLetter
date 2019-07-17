c72270f89ace265bac49f76696c7809246b827b4
edwardsosnoski <edward.sosnoski@gmail.com>
2019-07-17 08:10:34 -0400

    initial commit

---
+++ AHCB_CapitalizeEveryOtherLetter/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+
+namespace AHCB_CapitalizeEveryOtherLetter
+{
+    class Program
+    {
+        public static string AcceptUserInput()
+        {
+            Console.WriteLine("Enter any text.");
+            string userInput = Console.ReadLine();
+            return userInput;
+        }
+
+        public static string CapitalizeEveryOtherLetter(string userInput)
+        {
+            StringBuilder capitalizeEveryOtherLetter = new StringBuilder();
+
+            for (int i = 0; i < userInput.Length; i++)
+            {
+                if(i % 2 == 0)
+                {
+                    string lowercaseLetter = userInput[i].ToString().ToLower();
+                    capitalizeEveryOtherLetter.Append(lowercaseLetter);
+                }
+                else
+                {
+                    string capitalizeLetter = userInput[i].ToString().ToUpper();
+                    capitalizeEveryOtherLetter.Append(capitalizeLetter);
+                }
+            }
+
+            return capitalizeEveryOtherLetter.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            string userInput = AcceptUserInput();
+            Console.WriteLine($"This is what you entered: {userInput}");
+
+            string spongebobMeme = CapitalizeEveryOtherLetter(userInput);
+            Console.WriteLine($"This is what you entered, but sarcastic: {spongebobMeme}");
+        }
+    }
+}
